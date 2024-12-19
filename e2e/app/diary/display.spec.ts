5ac0f65be6b7f00c000937ba35a71c7267399114
hayatohanaoka <hayato.hanaoka@uzabase.com>
2024-12-20 00:47:24 +0900

    dev done: create diary top page

--- e2e/app/diary/display.spec.ts
+++ e2e/app/diary/display.spec.ts
@@ -3,31 +3,32 @@ import { test, expect } from "@playwright/test";
 test("ユーザーはアプリケーションのタイトルとして「日記帳アプリ」と表示されているのを見ることができる", async ({ page }) => {
   await page.goto("/diary");
   const expectedText = "日記帳アプリ";
-
+  
   const target = await page.locator("div h1")
   await expect(target).toHaveText(expectedText);
 })
 
 test("ユーザーはタイトル下部のリンクから適切なページに遷移することができる", async ({ page }) => {
-	const expectedShowPageLinkText   = "日記の閲覧";
-	const expectedCreatePageLinkText = "日記の新規作成";
-	const expectedDeletePageLinkText = "日記の削除";
-	const expectedUpdatePageLinkText = "日記の編集";
+	const expectedShowPageLinkText   = "閲覧";
+	const expectedCreatePageLinkText = "作成";
+	const expectedDeletePageLinkText = "削除";
+	const expectedUpdatePageLinkText = "編集";
+	
+	const expectedShowLinkUrl   = "/diary/show";
+	const expectedCreateLinkUrl = "/diary/create";
+	const expectedDeleteLinkUrl = "/diary/delete";
+	const expectedUpdateLinkUrl = "/diary/update";
+	
+	await page.goto("/diary");
+	const showDiaryLinkElem   = await page.locator("[data-test-id='showLink']");
+	const createDiaryLinkElem = await page.locator("[data-test-id='createLink']");
+	const deleteDiaryLinkElem = await page.locator("[data-test-id='deleteLink']");
+	const updateDiaryLinkElem = await page.locator("[data-test-id='updateLink']");
 
-	const expectedShowLinkUrl   = "日記の閲覧";
-	const expectedCreateLinkUrl = "日記の新規作成";
-	const expectedDeleteLinkUrl = "日記の削除";
-	const expectedUpdateLinkUrl = "日記の編集";
-
-	const showDiaryLinkElem   = await page.locator("a[data-test-id='show-diary-link']");
-	const createDiaryLinkElem = await page.locator("a[data-test-id='update-diary-link']");
-	const deleteDiaryLinkElem = await page.locator("a[data-test-id='delete-diary-link']");
-	const updateDiaryLinkElem = await page.locator("a[data-test-id='update-diary-link']");
-
-	await expect(showDiaryLinkElem).toHaveValue(expectedShowPageLinkText);
-	await expect(createDiaryLinkElem).toHaveValue(expectedCreatePageLinkText);
-	await expect(deleteDiaryLinkElem).toHaveValue(expectedDeletePageLinkText);
-	await expect(updateDiaryLinkElem).toHaveValue(expectedUpdatePageLinkText);
+	await expect(showDiaryLinkElem).toHaveText(expectedShowPageLinkText);
+	await expect(createDiaryLinkElem).toHaveText(expectedCreatePageLinkText);
+	await expect(deleteDiaryLinkElem).toHaveText(expectedDeletePageLinkText);
+	await expect(updateDiaryLinkElem).toHaveText(expectedUpdatePageLinkText);
 
 	await expect(showDiaryLinkElem).toHaveAttribute("href", expectedShowLinkUrl);
 	await expect(createDiaryLinkElem).toHaveAttribute("href", expectedCreateLinkUrl);
--- src/app/diary/page.tsx
+++ src/app/diary/page.tsx
@@ -2,12 +2,25 @@
 
 import Form from 'next/Form';
 import { Header } from "../components/header";
+import Link from 'next/link';
 
 
 export default function topPage() {
   return (
-		<div>
-			<Header />
-		</div>
+    <div>
+      <Header />
+      <div>
+        <Link href="/diary/show" data-test-id="showLink">閲覧</Link>
+      </div>
+      <div>
+        <Link href="/diary/create" data-test-id="createLink">作成</Link>
+      </div>
+      <div>
+        <Link href="/diary/delete" data-test-id="deleteLink">削除</Link>
+      </div>
+      <div>
+        <Link href="/diary/update" data-test-id="updateLink">編集</Link>
+      </div>
+    </div>
   )
 }
