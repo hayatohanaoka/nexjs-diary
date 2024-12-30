ba2403caf07d05c4d0f88590f19981f421f582c2
hayatohanaoka <hayato.hanaoka@uzabase.com>
2024-12-31 03:25:12 +0900

    e2e done: Test for show page

--- e2e/app/diary/show/display.spec.ts
+++ e2e/app/diary/show/display.spec.ts
@@ -1,6 +1,18 @@
 import { test, expect } from "@playwright/test";
 import { assertHeader } from "../../../common/common";
 
-test("ユーザーはアプリケーションのタイトルとして「日記帳アプリ」と表示されているのを見ることができる",async ({ page }) => {
-  await assertHeader("/diary/create", "日記帳アプリ", page);
+test.describe("日記の閲覧画面の表示テスト", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/diary/show");
+  })
+
+  test("ユーザーはアプリケーションのタイトルとして「日記帳アプリ」と表示されているのを見ることができる", async ({ page }) => {
+    await assertHeader("/diary/create", "日記帳アプリ", page);
+  })
+  test("ユーザーは日記の項目ヘッダーが表示されているのを見ることができる", async ({ page }) => {
+    const expectedHeaderItems = ["日付", "タイトル", "本文"]
+    
+    const targetTableHeader = await page.locator("th[data-test-id='diary-header'] tr").allInnerTexts();
+    expect(targetTableHeader).toEqual(expectedHeaderItems);
+  })
 })
