603365ea05937fb0be4cc6b2526f4d46fd5c7988
hayatohanaoka <hayato.hanaoka@uzabase.com>
2025-01-10 22:37:14 +0900

    e2e: Refactor test

--- e2e/app/diary/display.spec.ts
+++ e2e/app/diary/display.spec.ts
@@ -1,35 +1,42 @@
 import { test, expect } from "@playwright/test";
 import assert from "node:assert";
 import { assertHeader } from "../../common/common";
+import { describe } from "node:test";
 
-test("ユーザーはアプリケーションのタイトルとして「日記帳アプリ」と表示されているのを見ることができる",async ({ page }) => {
-  await assertHeader("/diary", "日記帳アプリ", page);
-})
+describe("日記アプリのトップページの表示テスト", () => {
+	test.beforeEach(async ({ page }) => {
+		await page.goto("/diary");
+	})
 
-test("ユーザーはタイトル下部のリンクから適切なページに遷移することができる", async ({ page }) => {
-	const expectedShowPageLinkText   = "閲覧";
-	const expectedCreatePageLinkText = "作成";
-	const expectedDeletePageLinkText = "削除";
-	const expectedUpdatePageLinkText = "編集";
+	test("ユーザーはアプリケーションのタイトルとして「日記帳アプリ」と表示されているのを見ることができる", async ({ page }) => {
+		await assertHeader("/diary", "日記帳アプリ", page);
+	})
+
+	test("ユーザーはタイトル下部のリンクから適切なページに遷移することができる", async ({ page }) => {
+		const expectedShowPageLinkText   = "閲覧";
+		const expectedCreatePageLinkText = "作成";
+		const expectedDeletePageLinkText = "削除";
+		const expectedUpdatePageLinkText = "編集";
+		
+		const expectedShowLinkUrl   = "/diary/show";
+		const expectedCreateLinkUrl = "/diary/create";
+		const expectedDeleteLinkUrl = "/diary/delete";
+		const expectedUpdateLinkUrl = "/diary/update";
+		
+		await page.goto("/diary");
+		const showDiaryLinkElem   = await page.locator("[data-test-id='showLink']");
+		const createDiaryLinkElem = await page.locator("[data-test-id='createLink']");
+		const deleteDiaryLinkElem = await page.locator("[data-test-id='deleteLink']");
+		const updateDiaryLinkElem = await page.locator("[data-test-id='updateLink']");
 	
-	const expectedShowLinkUrl   = "/diary/show";
-	const expectedCreateLinkUrl = "/diary/create";
-	const expectedDeleteLinkUrl = "/diary/delete";
-	const expectedUpdateLinkUrl = "/diary/update";
+		await expect(showDiaryLinkElem).toHaveText(expectedShowPageLinkText);
+		await expect(createDiaryLinkElem).toHaveText(expectedCreatePageLinkText);
+		await expect(deleteDiaryLinkElem).toHaveText(expectedDeletePageLinkText);
+		await expect(updateDiaryLinkElem).toHaveText(expectedUpdatePageLinkText);
 	
-	await page.goto("/diary");
-	const showDiaryLinkElem   = await page.locator("[data-test-id='showLink']");
-	const createDiaryLinkElem = await page.locator("[data-test-id='createLink']");
-	const deleteDiaryLinkElem = await page.locator("[data-test-id='deleteLink']");
-	const updateDiaryLinkElem = await page.locator("[data-test-id='updateLink']");
-
-	await expect(showDiaryLinkElem).toHaveText(expectedShowPageLinkText);
-	await expect(createDiaryLinkElem).toHaveText(expectedCreatePageLinkText);
-	await expect(deleteDiaryLinkElem).toHaveText(expectedDeletePageLinkText);
-	await expect(updateDiaryLinkElem).toHaveText(expectedUpdatePageLinkText);
-
-	await expect(showDiaryLinkElem).toHaveAttribute("href", expectedShowLinkUrl);
-	await expect(createDiaryLinkElem).toHaveAttribute("href", expectedCreateLinkUrl);
-	await expect(deleteDiaryLinkElem).toHaveAttribute("href", expectedDeleteLinkUrl);
-	await expect(updateDiaryLinkElem).toHaveAttribute("href", expectedUpdateLinkUrl);
+		await expect(showDiaryLinkElem).toHaveAttribute("href", expectedShowLinkUrl);
+		await expect(createDiaryLinkElem).toHaveAttribute("href", expectedCreateLinkUrl);
+		await expect(deleteDiaryLinkElem).toHaveAttribute("href", expectedDeleteLinkUrl);
+		await expect(updateDiaryLinkElem).toHaveAttribute("href", expectedUpdateLinkUrl);
+	})
 })
--- e2e/app/diary/edit/display.spec.ts
+++ e2e/app/diary/edit/display.spec.ts
@@ -1,7 +1,35 @@
 import { test, expect } from "@playwright/test";
 import { assertHeader } from "../../../common/common";
 
-test("ユーザーはアプリケーションのタイトルとして「日記帳アプリ」と表示されているのを見ることができる",async ({ page }) => {
-  await assertHeader("/diary/create", "日記帳アプリ", page);
+test.describe("日記の編集画面の表示テスト", () => {
+  test.describe("正常系" , () => {
+    test.beforeEach(async ({ page }) => {
+      await page.goto("/diary/edit/1");
+    })
+
+    test("ユーザーはアプリケーションのタイトルとして「日記帳アプリ」と表示されているのを見ることができる",async ({ page }) => {
+      await assertHeader("/diary/edit/1", "日記帳アプリ", page);
+    })
+
+    test("ユーザーは編集前の日記の内容がデフォルトで入力されているのを見ることができる", async ({ page }) => {
+      const expectedTitleText = "固定タイトル";
+      const expectedBody = "固定本文";
+      const expectedYear = "2020"
+      const expectedMonth = "1"
+      const expectedDay = "1";
+
+      const targetTitle = await page.locator("form div[data-test-id='diary-title'] input");
+      const targetBody = await page.locator("form div[data-test-id='diary-main'] textarea");
+      const targetYearPulldown = await page.locator("form select[data-test-id='diary-year']");
+      const targetMonthPulldown = await page.locator("form select[data-test-id='diary-month']");
+      const targetDayPulldown = await page.locator("form select[data-test-id='diary-day']");
+
+      await expect(targetTitle).toHaveValue(expectedTitleText);
+      await expect(targetBody).toHaveValue(expectedBody);
+      await expect(targetYearPulldown).toHaveValue(expectedYear);
+      await expect(targetMonthPulldown).toHaveValue(expectedMonth);
+      await expect(targetDayPulldown).toHaveValue(expectedDay);
+    })
+  })
 })
 
