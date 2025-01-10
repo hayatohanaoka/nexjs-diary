aa5b1c9e6c4554aafe920de449ddabc6543d6c38
hayatohanaoka <hayato.hanaoka@uzabase.com>
2025-01-10 13:19:54 +0900

    e2e: Fix locator

--- e2e/app/diary/show/id/display.spec.ts
+++ e2e/app/diary/show/id/display.spec.ts
@@ -3,25 +3,27 @@ import { assertHeader } from "../../../../common/common";
 
 test.describe("日記の閲覧画面の表示テスト", () => {
 	test.beforeEach(async ({ page }) => {
-		await page.goto("/diary/show");
+		await page.goto("/diary/show/1");
 	})
-  
+
 	test("ユーザーはアプリケーションのタイトルとして「日記帳アプリ」と表示されているのを見ることができる", async ({ page }) => {
 		await assertHeader("/diary/create", "日記帳アプリ", page);
 	})
 
 	test("ユーザーは「id=1」の日記の内容を閲覧することができる", async ({ page }) => {
-		const expectedTitle = "固定タイトル";
+		const expectedId = "Diary ID: 1";
+		const expectedTitle = "Title: 固定タイトル";
 		const expectedBody = "固定本文";
-		const expectedDate = "2020-01-01";
+		const expectedDate = "Write date: 2020-01-01";
 
-		const targetTitle = await page.locator("h1").innerText();
-		const targetBody = await page.locator("p[data-test-id='diary-body']").innerText();
-		const targetDate = await page.locator("p[data-test-id='diary-date']").innerText();
+		const targetId = await page.locator("h3[data-test-id='diary-id']").innerText();
+		const targetTitle = await page.locator("h3[data-test-id='diary-title']").innerText();
+		const targetBody = await page.locator("h3[data-test-id='diary-body']").innerText();
+		const targetDate = await page.locator("h3[data-test-id='diary-date']").innerText();
 
+		expect(targetId).toEqual(expectedId);
 		expect(targetTitle).toEqual(expectedTitle);
 		expect(targetBody).toEqual(expectedBody);
 		expect(targetDate).toEqual(expectedDate);
 	})
-  
 });
